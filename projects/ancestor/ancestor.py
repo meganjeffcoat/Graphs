02d93329eca9e82c1b562a9a5a4e150e5ea15ab6
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-10-08 14:37:19 -0500

    I was incorrect, requires a stack so replaced queue with stack class

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -5,20 +5,21 @@
 # -1 if no parents
 # ancestors - vertex
 # parent - edges?
+# use a cache?
 
 
-class Queue():
+class Stack():
     def __init__(self):
-        self.queue = []
-    def enqueue(self, value):
-        self.queue.append(value)
-    def dequeue(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
         if self.size() > 0:
-            return self.queue.pop(0)
+            return self.stack.pop()
         else:
             return None
     def size(self):
-        return len(self.queue)
+        return len(self.stack)
 
 def earliest_ancestor(ancestors, starting_node):
     pass
