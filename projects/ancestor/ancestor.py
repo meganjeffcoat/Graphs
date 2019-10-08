1a2ed7edb9a023f3203f27c764173aa0b10b1127
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-10-08 16:35:57 -0500

    ancestor complete MVP

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,6 +1,6 @@
 # (parent, child) pairs - set()
-# individual - starting_node
-# earliest - destination_node
+# individual - starting_vertex
+# earliest - destination_vertex
 # farthest distance from the input individual - path
 # -1 if no parents
 # ancestors - vertex
@@ -22,4 +22,34 @@ class Stack():
         return len(self.stack)
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = {}
+    for pair in ancestors:
+        if pair[1] in graph:
+            graph[pair[1]].append(pair[0])
+        else:
+            graph[pair[1]] = [pair[0]]
+        
+    stack = Stack()
+    result = starting_node
+    visited = set()
+    stack.push([starting_node])
+    a_len = 0
+
+    if starting_node not in graph:
+        return -1
+    while stack.size() > 0:
+        path = stack.pop()
+        node = path[-1]
+        if node in graph:
+            for parent in graph[node]:
+                new_path = list(path)
+                new_path.append(parent)
+                stack.push(new_path)
+                if new_path == 0:
+                    result = min(result, parent)
+                elif len(new_path) > a_len:
+                    result = parent
+                    a_len = len(new_path)
+    return result
+
+    
