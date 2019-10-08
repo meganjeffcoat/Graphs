7092393fb8a718a2cf991b72ca0ff6536135ef80
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-10-08 14:23:07 -0500

    pulled key words our of ancestor readme

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,9 @@
+# (parent, child) pairs - set()
+# individual - starting_node
+# earliest - destination_node
+# -1 if no parents
+# ancestors - vertex
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
