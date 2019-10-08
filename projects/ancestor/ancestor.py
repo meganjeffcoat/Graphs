0786d40da2937ae1e1e3edb0a9154a7e856fb415
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-10-08 14:27:57 -0500

    brought in queue class

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,9 +1,24 @@
 # (parent, child) pairs - set()
 # individual - starting_node
 # earliest - destination_node
+# farthest distance from the input individual - path
 # -1 if no parents
 # ancestors - vertex
+# parent - edges?
 
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
 def earliest_ancestor(ancestors, starting_node):
     pass
